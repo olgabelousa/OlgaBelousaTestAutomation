fe1d5977e08951dbf371da0b33c446384604f315
olgabelousa <o.belousa@gmail.com>
2022-07-26 20:18:39 +0300

    FirstClassHomework

---
+++ src/main/java/homework/FirstClass.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        String country="Latvija";
+        int numberofcitizens=2000000;
+        int area=400000000;
+        String capitalcity="Riga";
+        String language="Latvian";
+        boolean isoficialeucountry=true;
+        char currency='€';
+        System.out.println("Nav otras tādas zemes kā Latvija. Pašas skaistākās lauku sētas ar kupliem ozoliem, smaržojošām liepām un vējā maigi šalcošiem bērziem vasarā sagaida mūs. Ziemā būtība jau nemainās, tikai koki ir ar kailiem zariem, bet tikpat labsirdīgi kā vienmēr.");
+
+    }
+}
